bdafe0badb7a6b5708a7dd92fc374ea1554e72f9
yasbir7142 <yasbiryavuz7142@gmail.com>
2021-12-24 19:44:42 +0100

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,5 +3,6 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
+        System.out.println("About to share project on GitHub");
     }
 }
